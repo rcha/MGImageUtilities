5249aa48d2e512aae0b587fb2b08209bdb7abeb9
Roberto Cha <roberto.cha@teradek.com>
2013-11-05 14:35:03 -0800

    Fixed warning

--- Classes/UIImage+ProportionalFill.m
+++ Classes/UIImage+ProportionalFill.m
@@ -104,7 +104,7 @@
 		// Try older method.
 		CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
 		CGContextRef context = CGBitmapContextCreate(NULL, fitSize.width, fitSize.height, 8, (fitSize.width * 4), 
-													 colorSpace, kCGImageAlphaPremultipliedLast);
+													 colorSpace, (CGBitmapInfo)kCGImageAlphaPremultipliedLast);
 		CGImageRef sourceImg = CGImageCreateWithImageInRect([self CGImage], sourceRect);
 		CGContextDrawImage(context, destRect, sourceImg);
 		CGImageRelease(sourceImg);
